f99848b5e4fb6c1b8d197dafb1f74b4078d71da3
zjc <415557592@qq.com>
2018-02-28 13:12:27 +0800

    Group 的增删改查

--- trunk/Application/Controller/Admin/GroupController.class.php
+++ trunk/Application/Controller/Admin/GroupController.class.php
@@ -6,14 +6,14 @@
  * Date: 2018/2/28
  * Time: 10:16
  */
-class GroupController extends PlatformController
+class GroupController extends Controller
 {
     private $obj;
     public function __construct()
     {
-        parent::__construct();
+//        parent::__construct();
         @session_start();
-        $this->obj = ObjFactory::createObj('HomeModel');
+        $this->obj = ObjFactory::createObj('GroupModel');
     }
     public function index(){
         //接受数据
@@ -25,4 +25,48 @@ class GroupController extends PlatformController
         $this->display('index');
 
     }
-}
+    public function delete(){
+        //接受数据
+        $id=$_GET['id'];
+        //处理数据
+        $this->obj->getdelete($id);
+        //显示页面
+        $this->redirect('index.php?p=Admin&c=Group&a=index','删除成功',3);
+    }
+    public function edit(){
+        //分支
+        if ($_SERVER['REQUEST_METHOD']=='POST'){
+            //接受数据
+            $data=$_POST;
+            //处理数据
+           $res= $this->obj->getedit_save($data);
+            //显示页面
+            if ($res===false){
+                $this->redirect("index.php?p=Admin&c=Group&a=edit&id='{$data['id']}'",'修改失败'.$this->obj->getError(),3);
+            }
+            $this->redirect('index.php?p=Admin&c=Group&a=index','修改成功',3);
+
+        }else{
+            //接受数据
+            $id=$_GET['id'];
+            //处理数据
+            $row=$this->obj->getedit($id);
+            //分配数据
+            $this->assign('row',$row);
+            //显示页面
+            $this->display('edit');
+        }
+    }
+    public function add(){
+            //接受数据
+            $data=$_POST;
+            //处理数据
+           $res= $this->obj->getadd($data);
+            //显示页面分支
+        if ($res===false){
+            $this->redirect('index.php?p=Admin&c=Group&a=index','添加失败'.$this->ogb->getError(),3);
+        }else{
+        $this->redirect('index.php?p=Admin&c=Group&a=index','添加成功',3);
+    }
+    }
+    }
--- trunk/Application/Model/GroupModel.class.php
+++ trunk/Application/Model/GroupModel.class.php
@@ -9,8 +9,45 @@
 class GroupModel extends Model
     {
         public function getAll(){
+            //准备sql
             $sql="select * from `group`";
+          //处理sql 返回值
            return $this->pdo->fetchAll($sql);
-
+        }
+        public function getdelete($id){
+            //准备sql
+            $sql="delete from `group` where id={$id}";
+            //执行sql
+            $this->pdo->execute($sql);
+        }
+        public function getedit($id){
+            //准备sql
+            $sql="select * from `group` where id={$id}";
+            //执行sql
+            $res=$this->pdo->fetchRow($sql);
+            //返回值
+            return $res;
+        }
+        public function getedit_save($data){
+            //判断健壮性
+            if(empty($data['name'])){
+                $this->error='请填写名字';
+                return false;
+            }
+            //准备sql
+            $sql="update `group` set `name`='{$data['name']}' where id={$data['id']}";
+            //执行sql
+            $this->pdo->execute($sql);
+        }
+        public function getadd($data){
+            //判断健壮性
+            if (empty($data['name'])){
+                $this->error='请填写名称';
+                return false;
+            }
+            //准备sql
+            $sql="insert into `group` set `name`='{$data['name']}'";
+            //执行sql
+            $this->pdo->execute($sql);
         }
     }
